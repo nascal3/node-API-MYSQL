9d87bcf0225235f009a352cfe4160e4d3d949dbe
nascal3 <nascal3@gmail.com>
2018-11-08 03:25:51 +0300

    created middleware to check if valid token exists in header

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const users = require('../routes/users');
+const login = require('../routes/login');
+const articles = require('../routes/articles');
 
 module.exports = function (app) {
   app.use(express.json());
   app.use('/api/users', users);
+  app.use('/api/articles', articles);
+  app.use('/api/login', login);
 };
