51f3fadaec9022f1249b0495ec10601011f62bee
nascal3 <nascal3@gmail.com>
2018-12-14 04:31:53 +0300

    changed login process to use mysql2 connection promise syntax

--- routes/login.js
+++ routes/login.js
@@ -1,37 +1,33 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const generateToken = require('../models/usersTokenGen');
-const {Connection} = require('../startup/db');
+const Connection = require('../startup/db');
 const router = express.Router();
 
 // DO THE LOGIN PROCESS
 router.post('/', async(req, res) => {
 
   const params = [ req.body.username];
-  await Connection.query('SELECT id, first_name, email, password, role FROM users WHERE email = ?', params, (error, results, fields) => {
-    if (error) return res.status(400).send(error);
-    if (results.length <= 0) return res.status(400).send('Invalid email or password');
+  const [rows, fields] = await Connection.execute('SELECT id, first_name, email, password, role FROM users WHERE email = ?', params);
 
-    auth(results);
-
-  });
-
-  // AUTHENTICATE PASSWORDS AND GENERATE TOKEN
-  const auth = async (results) => {
-    const validPassword = await bcrypt.compare(req.body.password, results[0].password);
+  if (rows.length <= 0) {
+    return res.status(400).send('Invalid email or password');
+  } else {
+    // AUTHENTICATE PASSWORDS AND GENERATE TOKEN
+    const validPassword = await bcrypt.compare(req.body.password, rows[0].password);
     if (!validPassword) return res.status(400).send('Invalid email or password');
 
-    let id = results[0].id;
-    let first_name = results[0].first_name;
-    let role = results[0].role;
-    let email = results[0].email;
+    let id = rows[0].id;
+    let first_name = rows[0].first_name;
+    let role = rows[0].role;
+    let email = rows[0].email;
     const token = generateToken(id, first_name, role, email);
     const userToken = {
       userToken: token
     };
 
     return res.header('x-auth-token', token).status(200).send(userToken);
-  };
+  }
 });
 
 module.exports = router;
