6a825ee7b0aa43c84b25861b1a7fad73460677a7
devlap <nascal3@gmail.com>
2018-12-14 15:45:59 +0300

    edited token error message

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const config = require('config');
 
 const  auth = (req, res, next) => {
   const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied, no token!');
+  if (!token) return res.status(401).send('Access denied, no token provided!');
 
   try {
     req.user = jwt.verify(token, config.get('jwtPrivateKey'));
