882e4484cba22afd02b01b6e46d3c78a47997528
nascal3 <nascal3@gmail.com>
2018-12-14 04:30:59 +0300

    added mysql2 connection promise export correctly

--- startup/db.js
+++ startup/db.js
@@ -12,12 +12,4 @@ const connection = mysql.createPool(options);
 
 const Connection = connection.promise();
 
-Connection.execute('select * from users').then((results) => {
-  if (results) {
-    console.log('DB connected successfully');
-  }
-}).catch(err => {
-    console.error(`An error occurred while connecting to the DB: ${err} `);
-});
-
-module.exports = connection;
+module.exports = Connection;
