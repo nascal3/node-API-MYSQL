5335af7996a40da81b8682ab07c8d3d4486078d9
devlap <nascal3@gmail.com>
2018-12-15 00:20:07 +0300

    created sequelize articles model

---
+++ models/articles.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../startup/db');
+const UserModel = require('./users');
+
+const article = sequelize.define('article', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  user_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: UserModel,
+      key: UserModel.id
+    }
+  },
+  title: Sequelize.TEXT,
+  article_content: Sequelize.TEXT
+});
+
+module.exports = article;
