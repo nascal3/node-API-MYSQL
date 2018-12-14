3de5302fa17932096031e98b7402717f31f1431a
devlap <nascal3@gmail.com>
2018-12-15 00:19:13 +0300

    used sequelize syntax to for login code

--- routes/login.js
+++ routes/login.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const generateToken = require('../models/usersTokenGen');
-const Connection = require('../startup/db');
+const User = require('../models/users');
 const router = express.Router();
 
 // DO THE LOGIN PROCESS
 router.post('/', async(req, res) => {
 
-  const params = [ req.body.username];
-  const [rows, fields] = await Connection.execute('SELECT id, first_name, email, password, role FROM users WHERE email = ?', params);
+  let results = await User.findOne({
+    where: {
+      email: req.body.username
+    }
+  });
 
-  if (rows.length <= 0) {
+  if ( results == null) {
     return res.status(400).send('Invalid email or password');
   } else {
     // AUTHENTICATE PASSWORDS AND GENERATE TOKEN
-    const validPassword = await bcrypt.compare(req.body.password, rows[0].password);
+    const validPassword = await bcrypt.compare(req.body.password, results.dataValues.password);
     if (!validPassword) return res.status(400).send('Invalid email or password');
 
-    let id = rows[0].id;
-    let first_name = rows[0].first_name;
-    let role = rows[0].role;
-    let email = rows[0].email;
+    let id = results.dataValues.id;
+    let first_name = results.dataValues.first_name;
+    let role = results.dataValues.role;
+    let email = results.dataValues.email;
     const token = generateToken(id, first_name, role, email);
     const userToken = {
       userToken: token
