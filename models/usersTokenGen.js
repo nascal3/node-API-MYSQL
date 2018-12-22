69103b087af71f1f478b4dc076d5fe5fa0eab2c2
devlap <nascal3@gmail.com>
2018-12-22 17:59:22 +0300

    add users /authors name to the generated token

--- models/usersTokenGen.js
+++ models/usersTokenGen.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-const generateAuthToken = (id, first_name, role, email) => {
-   return jwt.sign({id: id, first_name: first_name, role: role, email: email}, config.get('jwtPrivateKey') );
+const generateAuthToken = (id, first_name, last_name, role, email) => {
+   return jwt.sign({id: id, name: first_name +' '+ last_name, role: role, email: email}, config.get('jwtPrivateKey') );
 };
 
 module.exports = generateAuthToken;
